54dc1e9bd72a69dfc5c8b37a34dd8ed03198b3b7
Alexander Stoimenov <me@astoimenov.eu>
2018-02-20 10:10:53 +0200

    Create SuspendAccountHolder.php

---
+++ src/Adyen/Service/ResourceModel/Markerplace/SuspendAccountHolder.php
@@ -0,0 +1,25 @@
+
+<?php
+
+namespace Adyen\Service\ResourceModel\Markerplace;
+
+class SuspendAccountHolder extends \Adyen\Service\AbstractResource
+{
+    protected $_requiredFields = [
+        'accountHolderCode',
+    ];
+
+    /**
+     * SuspendAccountHolder constructor.
+     *
+     * @param \Adyen\Service $service
+     */
+    public function __construct(\Adyen\Service $service)
+    {
+        $this->_service = $service;
+        $this->_endpoint = $service->getClient()
+          ->getConfig()
+          ->get('marketplaceEndpoint').'/cal/services/Account/'.$service->getClient()
+          ->getApiMarketplaceVersion().'/suspendAccountHolder';
+    }
+}
